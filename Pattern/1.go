ac82ef1d71d317d2387550c4a49fcef8c98f693a
AkKiL <78695802+HeimanPictures@users.noreply.github.com>
2023-05-31 18:24:43 +0000

    Just a start

---
+++ Pattern/1.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	/*output
+		* * * *
+		 * * *
+		  * *
+		   *
+	*/
+	fmt.Print("Hello world!")
+}
---
+++ Pattern/2.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	/*output
+	1 2 3 4
+	1 2 3
+	1 2
+	1
+	*/
+	fmt.Print("Hello world!")
+}
