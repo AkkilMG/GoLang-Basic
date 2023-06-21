8a9259bfffa9561b3f9427cd624434f33e361e15
AkKiL <78695802+HeimanPictures@users.noreply.github.com>
2023-06-21 09:42:19 +0000

    Adding module 2 a

---
+++ Problem/Module2/largestPalindromeProduct.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+)
+
+func isPalindrome(n int) bool {
+	s := strconv.Itoa(n)
+	lenght := len(s)
+	for i:=0; i<lenght/2; i++ {
+		if(s[i] != s[lenght-i-1]) {
+			return false
+		}
+	}
+	return true
+}
+
+func largestPalindromeProduct() (int, int, int) {
+	lp := 0
+	var mul1, mul2 int
+	for i:=999; i>=100; i-- {
+		for j:=i; j>=100; j-- {
+			if i*j < lp { break }
+			if i*j > lp && isPalindrome(i*j) {
+				lp = i*j
+				mul1 = i
+				mul2 = j
+			}
+		}
+	}
+	return lp, mul1, mul2
+}
+
+func main() {
+	result, mul1, mul2 := largestPalindromeProduct()
+	fmt.Printf("The largest palindrome product is: %d\n", result)
+	fmt.Printf("The multiplicants are: %d and %d\n", mul1, mul2)
+}
